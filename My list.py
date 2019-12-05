fe58aa9e61a8e7b78dbf617d84d114869fd8b3fa
KilrK12 <57633024+KilrK12@users.noreply.github.com>
2019-12-05 11:22:56 -0700

    Bruh 2

--- My list.py
+++ My list.py
@@ -1,3 +1,8 @@
+"""
+Kenshin Rasmussen
+Reverse
+12/5/2019
+"""
 def reverseWords(word):
     rev = ""
     for i in range(1, len(word)):
@@ -11,3 +16,18 @@ def reverse(target):
         rev.append(target[-i])
     return rev
 print(reverse(["Dylan", "Cole", "Caleb", "Bruh"]))
+
+"""
+Kenshin Rasmussen
+Sum
+12/5/2019
+"""
+def sumOFList(targ):
+    length = len(targ)-1
+    new = 0
+    new = new + targ(i)
+    
+    return value
+print(sumOFList([8,4]))
+        
+        
